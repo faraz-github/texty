e64e46ed6065bef07949db1ada4379976e0802b2
farazahmad <farazahmad.email@gmail.com>
2023-08-15 23:36:57 +0530

    Add database types

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -1,6 +1,25 @@
 interface User {
+  id: string;
   name: string;
   email: string;
   image: string;
+}
+
+interface Chat {
   id: string;
+  messages: Message[];
+}
+
+interface Message {
+  id: string;
+  senderId: string;
+  receiverId: string;
+  text: string;
+  timestamp: number; // unix timestamp
+}
+
+interface FriendRequest {
+  id: string;
+  senderId: string;
+  receiverId: string;
 }
